86531e7b8aad25af408124a21e0d6506c923b848
MadsKodehode <mads.kodehode@gmail.com>
2023-01-22 18:12:31 +0100

    Fixed form submit

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
-//Path
-const path = require("path");
-
-//Connection to db
+//Import mongoose
+const mongoose = require("mongoose");
+//Import db connect function
 const dbConnect = require("./db/dbConnect");
-//Cors
+//Import cors
 const cors = require("cors");
+//Import cookie parser
 const cookieParser = require("cookie-parser");
-//Port
-const PORT = 3500;
+//Import environment variables
+require("dotenv").config();
+//Create express app
 const app = express();
+
+//PORT
+const PORT = process.env.PORT || 5000;
 
 //Database connect
 dbConnect();
@@ -19,11 +23,12 @@ dbConnect();
 app.use(cors());
 //Body parser
 app.use(express.json());
-
 //Form data handler
 app.use(express.urlencoded({ extended: false }));
 //Cookie Parser
 app.use(cookieParser());
+
+/////////ROUTES\\\\\\\\\\
 //Register route
 app.use("/register", require("./routes/register"));
 //Login route
@@ -33,5 +38,14 @@ app.use("/free-access", require("./routes/free-access"));
 //Auth access
 app.use("/auth-access", require("./routes/authorized-access"));
 
-//Listen for activity on port
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.all("/*", (req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+//Making sure we are connected to db before listening to port
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+
+  //Listen for activity on port
+  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+});
